43f164c889828e3c58290b4afe12352608924020
Adam Coldrick <adam.coldrick@codethink.co.uk>
2017-12-01 13:55:15 +0000

    Rough day 1 solution

---
+++ day1/next.js
@@ -0,0 +1,13 @@
+fs = require('fs');
+
+fs.readFile('day1/data/input', 'utf8', (err, data) => {
+  var result = 0;
+  data = data.trim();
+  for (var i = 0; i < data.length; i++) {
+    var next = (i + 1) % data.length;
+    if (data[i] === data[next]) {
+      result += +data[i];
+    }
+  }
+  console.log(result);
+});
